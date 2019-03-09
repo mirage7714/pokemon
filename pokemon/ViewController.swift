dd13107ae53980bc9e9db5d16e97f05610d281a9
Mirage Chung <mirage.chung@grindr.com>
2019-03-09 09:54:18 +0800

    Initial Commit

---
+++ pokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokemon
+//
+//  Created by miragechung on 2019/3/9.
+//  Copyright © 2019 miragechung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
